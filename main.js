5abc49a83aa12d2f715eedcfc0f916fcd742e750
Anton Styazhkin <rudana089@gmail.com>
2023-04-12 18:25:56 +0300

    Getting and displaying movies on the page

--- main.js
+++ main.js
@@ -9,7 +9,31 @@ const options = {
     },
 };
 
-fetch(url + 'top', options)
-    .then(res => res.json())
-    .then(json => console.log(json))
-    .catch(err => console.log(err));
+// fetch(url + 'top', options)
+// 	.then((res) => res.json())
+// 	.then((json) => console.log(json))
+// 	.catch((err) => console.log(err));
+
+const filmsWrapper = document.querySelector('.films');
+
+async function fetchAndRenderFilms() {
+    const response = await fetch(url + 'top', options);
+    const data = await response.json();
+    console.log(data);
+    console.log(data.films);
+
+    for (film of data.films) {
+        console.log(film);
+
+        const html = `<div class="card">
+                <img src=${film.posterUrlPreview} alt="Cover" class="card__img">
+                <h3 class="card__title">${film.nameRu}</h3>
+                <p class="card__year">${film.year}</p>
+                <p class="card__rate">Рейтинг: ${film.rating}</p>
+            </div>`;
+
+        filmsWrapper.insertAdjacentHTML('beforeend', html);
+    }
+}
+
+fetchAndRenderFilms().catch(err => console.log(err));
